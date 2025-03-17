193cffec2e6fc68f5f2bf1220ba6d03bd29b80e4
Michaeldestjerome <michaelmarie.blacher@gmail.com>
2025-03-17 10:19:27 +0000

    Modification du projet dans Codespaces

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ vendeur = Vendeur(1000, 2, 5000)
 
 liste_acheteurs = []
 for i in range(20):
-    liste_acheteurs.appeYnd(Acheteur(randint(1000, 5000), randint(2, 10), randint(5, 50)))
+    liste_acheteurs.append(Acheteur(randint(1000, 5000), randint(2, 10), randint(5, 50)))
 
 simulation_1_vendeur_n_acheteur(vendeur, liste_acheteurs)
